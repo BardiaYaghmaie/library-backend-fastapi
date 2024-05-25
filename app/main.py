c4f682c15360c0876e80a6b3264e3f33c75b9391
Bardia Yaghmaie <bardia_yaghmaie@mathdep.iust.ac.ir>
2024-05-25 14:50:06 +0330

    CORS middleware wildcard

--- app/main.py
+++ app/main.py
@@ -5,10 +5,23 @@ import schemas
 import crud
 from database import SessionLocal, engine
 from typing import List
+from fastapi.middleware.cors import CORSMiddleware
+
 
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+origins = [
+    "http://localhost",
+    "https://localhost"
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Dependency
 def get_db():
